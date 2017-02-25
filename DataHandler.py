38119d4d7b92fb9ee1cdb295dbffd5b64de21aac
James Ash <jamesash@jaxfire.co.uk>
2017-02-25 14:50:00 +0000

    Add live stream data capture

--- DataHandler.py
+++ DataHandler.py
@@ -2,15 +2,47 @@ class DataHandler(object):
 
     tweet_counter = []
 
+    spike_qualifier = 2.2
+
     def add_data(self, latest_velocity):
+
         self.tweet_counter.append(latest_velocity)
 
-        print("____TICK____")
-        mean = self.mean(self.tweet_counter)
-        print("Mean:" + str(mean))
-        if self.tweet_counter[len(self.tweet_counter) - 1] > mean:
-            print("Greater than the mean")
-        print(self.tweet_counter)
+        length = len(self.tweet_counter)
 
+        # get the latest mean average
+        mean = round(self.mean(self.tweet_counter), 2)
+
+        # get the last minute's value
+        current_value = self.tweet_counter[len(self.tweet_counter) - 1]
+
+        # calculate it's variance form the mean
+        variance_from_mean = round(current_value / mean, 2)
+
+        # check for a spike in the data
+        spike = False
+        if current_value > mean * self.spike_qualifier:
+            spike = True
+
+        # create the verbose string
+        minute_update = "Minute: " + str(length) + ", Num of tweets: " + str(self.tweet_counter[length - 1])\
+                        + ", Current mean: " + str(mean) + ", Variance: " + str(variance_from_mean)
+
+        # append some notifying text if the data is spiking
+        if spike:
+            minute_update += ", DATA SPIKE"
+
+        # write to the verbose file
+        with open("match_data_verbose.txt", "a") as file:
+            file.write(minute_update + "\n")
+
+        # write to the raw data file
+        with open("match_data_raw.txt", "a") as file:
+            file.write(str(self.tweet_counter[length - 1]) + "," + str(mean) + "," + str(variance_from_mean) + "\n")
+
+        # for live results
+        print("Running... \n" + minute_update)
+
+    # calculates the mean average
     def mean(self, numbers):
         return float(sum(numbers)) / max(len(numbers), 1)
--- Main.py
+++ Main.py
@@ -15,10 +15,11 @@ first_run = True
 
 
 def minute_tick():
-    threading.Timer(5.0, minute_tick).start()
+    threading.Timer(60.0, minute_tick).start()
     data_handler.add_data(tweet_listener.on_tick())
 
-threading.Timer(5.0, minute_tick).start()
+threading.Timer(60.0, minute_tick).start()
 
 twitter_stream = Stream(authoriser.get_auth(), tweet_listener)
-twitter_stream.filter(track=['#trump'])
+twitter_stream.filter(track=['#chelsea', '#cfc', '#cheswa', '#chelseafc', '#swanseacity', '#swanseacitylive',
+                             '#swansofficial', '#swans'])
